770364c96b7850040775efb0507c3b36fcd377fb
Nicolas <nicolasandreose@gmail.com>
2026-04-29 20:41:41 -0300

    feat: add helpers functions

---
+++ lib/helpers.ts
@@ -0,0 +1,18 @@
+// Pega a data formatada em Sexta, 2 de fevereiro
+export const getFormattedDate = () => {
+  const date = new Date();
+  let formattedDate = formatter.format(date);
+  formattedDate = formattedDate.replace("-feira", "");
+  formattedDate = capitalizeFirstLetter(formattedDate);
+  return formattedDate;
+};
+
+export const capitalizeFirstLetter = (string: string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+};
+
+const formatter = new Intl.DateTimeFormat("pt-BR", {
+  weekday: "long",
+  day: "numeric",
+  month: "long",
+});
