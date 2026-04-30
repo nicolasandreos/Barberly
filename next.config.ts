da71a6371e5f2f88fe93d8570fa5a9d697f2459c
Nicolas <nicolasandreose@gmail.com>
2026-04-29 22:06:34 -0300

    chore: ensure next are able to get images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+    domains: ["utfs.io"],
+  },
 };
 
 export default nextConfig;
