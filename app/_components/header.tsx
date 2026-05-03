db98822cca025973307e0ac093615d4377e330cb
Nicolas <nicolasandreose@gmail.com>
2026-05-03 13:26:22 -0300

    feat: onClick on logo redirects user to home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import MenuButton from "./menu-button";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header>
       <div className="flex w-full items-center justify-between border-b border-white/10 px-8 py-6">
-        <Image src="/barberly-slogan2.png" alt="logo" width={70} height={70} />
+        <Link href="/">
+          <Image
+            src="/barberly-slogan2.png"
+            alt="logo"
+            width={70}
+            height={70}
+          />
+        </Link>
         <MenuButton className="size-10" />
       </div>
     </header>
